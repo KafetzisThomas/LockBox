91d5bf5fb7243feefcb0ca89bd9d34442ed4853f
KafetzisThomas <tomkafetzis06@gmail.com>
2026-01-31 17:28:05 +0200

    Refactor templates to match PassManagerWeb

--- main.py
+++ main.py
@@ -26,15 +26,15 @@ app.include_router(items.router, prefix="/api/items", tags=["Items"])
 
 @app.get("/", include_in_schema=False)
 @app.get("/vault", include_in_schema=False)
-def vault(request: Request):
-    return templates.TemplateResponse("items/vault.html", {"request": request})
+def vault_page(request: Request):
+    return templates.TemplateResponse("items/vault.html", {"request": request, "title": "My Vault"})
 
 
 @app.get("/login", include_in_schema=False)
 def login_page(request: Request):
-    return templates.TemplateResponse("users/login.html", {"request": request})
+    return templates.TemplateResponse("users/login.html", {"request": request, "title": "Login"})
 
 
 @app.get("/register", include_in_schema=False)
 def register_page(request: Request):
-    return templates.TemplateResponse("users/register.html", {"request": request})
+    return templates.TemplateResponse("users/register.html", {"request": request, "title": "Register"})
